862613e5d7547c9864c7df489c0a597bff8d4310
Ther CN <b1431736437@163.com>
2022-07-22 21:50:31 +0800

    build glibc test

--- build_glibc.sh
+++ build_glibc.sh
@@ -1,10 +1,10 @@
 sudo apt update
-sudo apt install gcc-12-aarch64-linux-gnu axel -y
+sudo apt install gcc-10-aarch64-linux-gnu axel -y
 axel https://ftp.gnu.org/gnu/glibc/glibc-2.35.tar.gz
 tar xvf glibc-2.35.tar.gz
 mkdir build
 cd build
-../glibc-2.35/configure --prefix=/data/data/com.termux/files/usr CC="aarch64-linux-gnu-gcc-12" CXX="aarch64-linux-gnu-g++-12"
+../glibc-2.35/configure --prefix=/data/data/com.termux/files/usr CC="aarch64-linux-gnu-gcc-10" CXX="aarch64-linux-gnu-g++-10"
 make -j8
 sudo make install DESTDIR=$HOME/glibc
 cd $HOME
