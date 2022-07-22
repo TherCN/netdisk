6c1d50f1a65f515a135bd11968025f143df677e8
Ther CN <b1431736437@163.com>
2022-07-22 21:32:40 +0800

    build glibc test

--- build_glibc.sh
+++ build_glibc.sh
@@ -1,9 +1,10 @@
-. download_ndk.sh
+sudo apt update
+sudo apt install gcc-12-aarch64-linux-gnu axel -y
 axel https://ftp.gnu.org/gnu/glibc/glibc-2.35.tar.gz
 tar xvf glibc-2.35.tar.gz
 mkdir build
 cd build
-../glibc-2.35/configure --prefix=/data/data/com.termux/files/usr CC=$NDK_ROOT/gcc CXX=$NFK_ROOT/g++
+../glibc-2.35/configure --prefix=/data/data/com.termux/files/usr CC="aarch64-linux-gnu-gcc-12" CXX="aarch64-linux-gnu-g++-12"
 make -j8
 sudo make install DESTDIR=$HOME/glibc
 cd $HOME
--- download_ndk.sh
+++ download_ndk.sh
@@ -3,7 +3,6 @@ sudo apt install axel unzip -y
 axel https://dl.google.com/android/repository/android-ndk-r17c-linux-x86_64.zip?hl=zh_cn
 unzip -q android-ndk-r17c-linux-x64_64.zip
 export NDK_ROOT=$PWD/android-ndk-r17c
-cd $NDK_ROOT
-echo '$NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin/aarch64-linux-android-$(basename $0) --sysroot=/AppData/android-ndk-r17c/platforms/android-21/arch-arm64 -isystem $NDK_ROOT/sysroot/usr/include -isystem $NDK_ROOT/sysroot/usr/include/aarch64-linux-android $@'>gcc
-ln -s gcc g++
-chmod 755 gcc
+export PATH=$NDK_ROOT/toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/bin:toolchains/aarch64-linux-android-4.9/prebuilt/linux-x86_64/aarch64-linux-android/bin:$PATH
+export NDKCC="aarch64-linux-android-gcc -isysroot=$NDK_ROOT/sysroot"
+export NDKCXX="aarch64-linux-android-g++ -isysroot=$NDK_ROOT/sysroot"
