5101b4a445586efc28136a00186e701632e2b7ae
Ther CN <b1431736437@163.com>
2022-07-25 10:54:44 +0800

    build glibc test

--- build_glibc_test.sh
+++ build_glibc_test.sh
@@ -1,12 +1,21 @@
 sudo apt update
-sudo apt install gcc-10-aarch64-linux-gnu axel -y
-axel https://ftp.gnu.org/gnu/glibc/glibc-2.35.tar.gz
-tar xvf glibc-2.35.tar.gz
+sudo apt install gcc-10-aarch64-linux-gnu axel libgmp-dev libmpc-dev libmpfr-dev -y
+axel https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz
+tar xvf gcc-12.1.0.tar.xz
 mkdir build
-cd build
-../glibc-2.35/configure --host=aarch64-linux-android --build=aarch64-linux-android --prefix=/data/data/bin.mt.plus/files/term/usr CC="aarch64-linux-gnu-gcc-10" CXX="aarch64-linux-gnu-g++-10" --disable-multiarch
+cd gcc-12.1.0
+patch -p1 <../fix-termux.patch
+sudo cd /
+sudo mkdir -p /data/data/com.termux/files/usr/gnu/aarch64-linux-android
+sudo cd /data/data/com.termux/files/usr/gnu/aarch64-linux-android
+sudo ln -s /home/runner/work/netdisk/netdisk/glibc/data/data/com.termux/files/usr/gnu/usr/lib
+sudo ln -s /home/runner/work/netdisk/netdisk/include
+cd /home/runner/work/netdisk/netdisk/build
+../gcc-12.1.0/configure CC="aarch64-linux-gnu-gcc-10" CXX="aarch64-linux-gnu-g++-10" --prefix=/data/data/com.termux/files/usr/gnu --enable-languages=c,c++ --disable-multilib --host=aarch64-linux-android --build=aarch64-linux-android --enable-default-pie --enable-shared --disable-bootstrap --disable-libsanitizer --with-arch=armv8-a --with-system-zlib
 make -j8
-sudo make install DESTDIR=$HOME/glibc
+sudo make install DESTDIR=$HOME/gcc
 cd $HOME
-tar cJvf glibc-2.35-mt.tar.xz glibc
-mv glibc-2.35-mt.tar.xz /home/runner/work/netdisk/netdisk
+tar cJvf glibc-gcc.tar.xz gcc
+mv glibc-gcc.tar.xz /home/runner/work/netdisk/netdisk
+cd /home/runner/work/netdisk/netdisk
+aarch64-linux-gnu-gcc-10 a.c -v 2>glibc-gcc-includedir.tar.xz
