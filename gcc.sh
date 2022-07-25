580ef602147214218063f17ba69fbaef93b3b3d4
Ther CN <b1431736437@163.com>
2022-07-25 11:37:23 +0800

    build glibc test

--- gcc.sh
+++ gcc.sh
@@ -1,16 +1,12 @@
 sudo apt update
 sudo apt install gcc-10-aarch64-linux-gnu axel libgmp-dev libmpc-dev libmpfr-dev -y
-axel https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz
-tar xvf gcc-12.1.0.tar.xz
+axel https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz 1>/dev/null
+tar xf gcc-12.1.0.tar.xz
 mkdir build
 cd gcc-12.1.0
 patch -p1 <../fix-termux.patch
-sudo cd /
-sudo mkdir -p /data/data/com.termux/files/usr/gnu/aarch64-linux-android
-sudo cd /data/data/com.termux/files/usr/gnu/aarch64-linux-android
-sudo ln -s /usr/aarch64-linux-gnu/lib
-sudo ln -s /usr/aarch64-linux-gnu/include
-cd /home/runner/work/netdisk/netdisk/build
+bash mksysroot
+cd ../build
 ../gcc-12.1.0/configure CC="aarch64-linux-gnu-gcc-10" CXX="aarch64-linux-gnu-g++-10" --prefix=/data/data/com.termux/files/usr/gnu --enable-languages=c,c++ --disable-multilib --host=aarch64-linux-android --build=aarch64-linux-android --enable-default-pie --enable-shared --disable-bootstrap --disable-libsanitizer --with-arch=armv8-a --with-system-zlib
 make -j8
 sudo make install-strip DESTDIR=$HOME/gcc
