529859ec241c99c38f9e06895d433c5523e1f196
Ther CN <b1431736437@163.com>
2022-07-25 13:35:16 +0800

    build glibc test

--- gcc.sh
+++ gcc.sh
@@ -5,11 +5,14 @@ tar xf gcc-12.1.0.tar.xz
 mkdir build
 cd gcc-12.1.0
 patch -p1 <../fix-termux.patch
+cd ..
 bash mksysroot
 cd ../build
 ../gcc-12.1.0/configure CC="aarch64-linux-gnu-gcc-10" CXX="aarch64-linux-gnu-g++-10" --prefix=/data/data/com.termux/files/usr/gnu --enable-languages=c,c++ --disable-multilib --host=aarch64-linux-android --build=aarch64-linux-android --enable-default-pie --enable-shared --disable-bootstrap --disable-libsanitizer --with-arch=armv8-a --with-system-zlib
 make -j8
 sudo make install-strip DESTDIR=$HOME/gcc
+cd ..
+cp build/config.log glibc-gcc-optput.tar.xz
 cd $HOME
 tar cJvf glibc-gcc.tar.xz gcc
 mv glibc-gcc.tar.xz /home/runner/work/netdisk/netdisk
