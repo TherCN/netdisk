b6a82efe15d04d45f7a8484264866383df11b838
Ther CN <b1431736437@163.com>
2022-07-26 19:53:51 +0800

    build glibc

--- glibc.sh
+++ glibc.sh
@@ -3,12 +3,13 @@ sudo apt install gcc-10-aarch64-linux-gnu axel -y
 axel https://ftp.gnu.org/gnu/glibc/glibc-2.35.tar.gz
 tar xvf glibc-2.35.tar.gz
 mkdir build
+for file in glibc-patch/*
+do
+patch -p0 -i $file
+done
 cd build
 ../glibc-2.35/configure --host=aarch64-linux-android --build=aarch64-linux-android --prefix=/data/data/com.termux/files/usr/gnu CC="aarch64-linux-gnu-gcc-10" CXX="aarch64-linux-gnu-g++-10" --disable-multiarch
 make -j8
-sudo make install DESTDIR=$HOME/glibc
-cd $HOME
+sudo make install DESTDIR=/home/runner/work/netdisk/netdisk/glibc
+cd ..
 tar cJvf glibc-2.35-main.tar.xz glibc
-mv glibc-2.35-main.tar.xz /home/runner/work/netdisk/netdisk
-cd /home/runner/work/netdisk/netdisk
-bash gcc.sh
