04d72f47a936280f925cac2947e93ae6a1cc53ba
Ther CN <b1431736437@163.com>
2022-08-22 19:32:28 +0800

    build x86_64 gcc

--- gcc.sh
+++ gcc.sh
@@ -5,11 +5,10 @@ mkdir build
 cd gcc-12.1.0
 bash contrib/download_prerequisites
 sed -i 's|mabi.lp64=../lib64|mabi.lp64=../lib|' gcc/config/aarch64/t-aarch64-linux
-cd ..
 #bash mksysroot
 #sudo ln -s aarch64-linux-gnu-gcc-10 aarch64-linux-gnu-gcc
 #sudo ln -s aarch64-linux-gnu-g++-10 aarch64-linux-gnu-g++
-cd $OLDPWD/build
+cd ../build
 ../gcc-12.1.0/configure \
 --prefix=/usr \
 --disable-multilib \
