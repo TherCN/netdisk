fbbad4db743ce87fff464e535022dc5b9697375d
Ther CN <b1431736437@163.com>
2022-08-24 21:47:05 +0800

    update init.sh

--- File/init.sh
+++ File/init.sh
@@ -1,6 +1,7 @@
 sudo echo '[archlinuxcn]
 Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch'>>/etc/pacman.conf
+sudo sed -i "s|#[multiarch]|[multiarch]|g" /etc/pacman.conf
 sudo echo 'Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'>/etc/pacman.d/mirrorlist
 sudo pacman -Syu wqy-zenhei archlinuxcn-keyring chromium base base-devel git --noconfirm
-sudo pacman -S pikaur code mesa hmcl
+sudo pacman -S pikaur code mesa hmcl wine wine-mono wine-gecko winetricks
 pikaur -S java8-openjfx
