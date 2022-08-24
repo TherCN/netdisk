9fdb3bccb0731798c6e686bdb5727dd3941ffad6
Ther CN <b1431736437@163.com>
2022-08-25 07:37:27 +0800

    update init.sh

--- File/init.sh
+++ File/init.sh
@@ -1,7 +1,7 @@
-sudo echo '[archlinuxcn]
+echo '[archlinuxcn]
 Server = https://mirrors.bfsu.edu.cn/archlinuxcn/$arch'>>/etc/pacman.conf
-sudo sed -i "s|#[multiarch]|[multiarch]|g" /etc/pacman.conf
-sudo echo 'Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'>/etc/pacman.d/mirrorlist
-sudo pacman -Syu wqy-zenhei archlinuxcn-keyring chromium base base-devel git --noconfirm
-sudo pacman -S pikaur code mesa hmcl wine wine-mono wine-gecko winetricks
-pikaur -S java8-openjfx
+echo 'Server = https://mirrors.ustc.edu.cn/archlinux/$repo/os/$arch'>/etc/pacman.d/mirrorslist
+pacman -Syu wqy-zenhei archlinuxcn-keyring chromium base base-devel git --noconfirm
+pacman -S pikaur mesa hmcl wine wine-mono wine-gecko winetricks
+pacman -U https://thercn.github.io/netdisk/File/java8-openjfx-8.u202-4-x86_64.pkg.tar.zst
+reboot
