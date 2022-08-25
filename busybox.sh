a7670fded994265c99a1be4feac364ed46bc19c5
Ther CN <b1431736437@163.com>
2022-08-25 18:58:11 +0800

    add busybox.sh

--- busybox.sh
+++ busybox.sh
@@ -1,4 +1,9 @@
-git clone https://github.com/mirror/busybox/tree/1_35_stable busybox
+git clone https://github.com/mirror/busybox/ -b 1_35_stable busybox
 cd busybox
-make menuconfig
+make defconfig
 make -j16
+make install
+mkdir /home/runner/work/netdisk/netdisk/busybox
+cp -r _install/* /home/runner/work/netdisk/netdisk/busybox
+cd /home/runner/work/netdisk/netdisk
+tar cJvf busybox.tar.xz busybox
