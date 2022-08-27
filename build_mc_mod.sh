0cc71b5135baf1f85b1c16192917b84784c84578
Ther CN <b1431736437@163.com>
2022-08-27 13:04:45 +0800

    update build_mcmod.sh

--- build_mc_mod.sh
+++ build_mc_mod.sh
@@ -3,4 +3,5 @@ mkdir forge && cd forge
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2860/forge-1.12.2-14.23.5.2860-mdk.zip
 unzip *mdk.zip
 cd ../TheOneProbe
+sed -i "s|tehnut.info|maven.blamejared.com|g" build.gradle
 ./gradlew
