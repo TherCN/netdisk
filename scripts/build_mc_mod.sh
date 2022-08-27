6d9cf566187d8621aeb99c4b0b2ba46394de336c
Ther CN <b1431736437@163.com>
2022-08-27 14:11:17 +0800

    build minecraft mod

---
+++ scripts/build_mc_mod.sh
@@ -0,0 +1,9 @@
+cd /home/runner/work/netdisk/netdisk
+git clone https://github.com/McJtyMods/TheOneProbe -b 1.12
+cd TheOneProbe
+sed -i "s|tehnut.info/maven|maven.blamejared.com|g" build.gradle
+sed -i "s|http://maven.epoxide.xyz|maven.blamejared.com|g" build.gradle
+export JAVA_HOME=/usr/lib/jvm/temurin-8-jdk-amd64
+patch -p1 < ../Chinese.patch
+./gradlew jar
+cp build/libs/the*.jar /home/runner/work/netdisk/netdisk
