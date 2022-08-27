d503579b1bef7a552e1353ec90470cb57d441944
Ther CN <b1431736437@163.com>
2022-08-27 11:20:59 +0800

    add build_mcmod.sh

--- build_mc_mod.sh
+++ build_mc_mod.sh
@@ -1,3 +1,6 @@
-git clone https://github.com/McJtyMods/TheOneProbe
+git clone https://github.com/McJtyMods/TheOneProbe -b 1.12
+mkdir forge && cd forge
 wget https://maven.minecraftforge.net/net/minecraftforge/forge/1.12.2-14.23.5.2860/forge-1.12.2-14.23.5.2860-mdk.zip
-
+unzip *mdk.zip
+cd ../TheOneProbe
+./gradlew
