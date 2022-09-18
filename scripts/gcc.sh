612f35364f903f892d26b3a0e1d6d2a12f7900e3
Ther CN <b1431736437@163.com>
2022-09-18 09:59:32 +0800

    update gcc to 12.2

--- scripts/gcc.sh
+++ scripts/gcc.sh
@@ -1,26 +1,25 @@
-sudo apt install gcc-10 g++-10 libc6-dev axel -y 1>/dev/null
-axel https://ftp.gnu.org/gnu/gcc/gcc-12.1.0/gcc-12.1.0.tar.xz 1>/dev/null
-tar xf gcc-12.1.0.tar.xz
+sudo apt install gcc-10 g++-10 axel -y 1>/dev/null
+axel https://ftp.gnu.org/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz 1>/dev/null
+tar xf gcc-12.2.0.tar.xz
 mkdir build
-cd gcc-12.1.0
+cd gcc-12.2.0
 bash contrib/download_prerequisites
 patch -p1 < ../fix.patch
 #bash mksysroot
 #sudo ln -s aarch64-linux-gnu-gcc-10 aarch64-linux-gnu-gcc
 #sudo ln -s aarch64-linux-gnu-g++-10 aarch64-linux-gnu-g++
 cd ../build
-../gcc-12.1.0/configure \
+../gcc-12.2.0/configure \
 --prefix=/usr \
---host=x86_64-linux-gnu --target=x86_64-linux-gnu \
+--host=aarch64-linux-gnu --target=aarch64-linux-gnu \
 --disable-multilib \
 --enable-default-pie \
 --disable-shared \
 --disable-bootstrap \
---enable-static \
---disable-rpath LDFLAGS_FOR_BUILD="-static" LDFLAGS="-static"
+--enable-static
 make -j16
 sudo make install-strip DESTDIR=$HOME/gcc
 bash /home/runner/work/netdisk/netdisk/binutils.sh
 cd $HOME
-tar cJvf glibc-gcc.tar.xz gcc
-mv glibc-gcc.tar.xz /home/runner/work/netdisk/netdisk
+tar cJvf gcc.tar.xz gcc
+mv gcc.tar.xz /home/runner/work/netdisk/netdisk
