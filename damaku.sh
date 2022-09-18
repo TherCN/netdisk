7815a14ffc3b1695f200892e3cb06218b8d935f0
Ther CN <b1431736437@163.com>
2022-09-19 07:17:30 +0800

    add damaku.sh

--- damaku.sh
+++ damaku.sh
@@ -1,4 +1,7 @@
 cd damaku
-sudo apt update && sudo apt install python3 python3-pip
+sudo apt update && sudo apt install python3 python3-pip -y
 pip3 install protobuf
-./danmaku.py
+./danmu.py
+mv *.xml ..
+cd ..
+mv *.xml damaku.xml
