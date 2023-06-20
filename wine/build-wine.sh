2dc7b16d90743988a4fb8ce3b82b7bc2f138c3f7
TherCN <85400103+TherCN@users.noreply.github.com>
2023-06-20 18:12:41 +0800

    Delete wine directory

--- wine/build-wine.sh
+++
@@ -1,7 +0,0 @@
-cd /home/runner/work/netdisk/netdisk/build-wine
-bash /home/runner/work/netdisk/netdisk/wine/linux-amd64-wine 1>/dev/null 2>/dev/null
-bash /home/runner/work/netdisk/netdisk/wine/builder_linux_amd64_runtime
-bash /home/runner/work/netdisk/netdisk/wine/builder_linux_amd64_wine
-cd /home/runner/work/netdisk/netdisk/build-wine
-tar cJf wine-${WINE_VERSION}-termux.tar.xz wine
-cp wine-${WINE_VERSION}-termux.tar.xz /home/runner/work/netdisk/netdisk/
